39a392a6f9daba39a6ee5fe4c0f54d28039eb777
Candillan <calvin.baker128@gmail.com>
2020-03-16 14:44:32 -0400

    please still work

---
+++ Development/Mod5/ToiletReaderFrontend/containers/Login.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import {Text, StyleSheet, View, TextInput, Button} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text> Login Form </Text>
+      <View>
+        <TextInput 
+          placeholder="Email" 
+        />
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Password"
+        />
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  //Check project repo for styles
+});
+
+export default App;
